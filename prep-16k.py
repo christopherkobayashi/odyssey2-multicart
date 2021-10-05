e7828af354cb2d9305e25604d924d8abee959782
Christopher RYU <software+github@disavowed.jp>
2021-10-05 22:24:54 +0900

    Refactoring makes this so very much clearer.

--- prep-16k.py
+++ prep-16k.py
@@ -21,48 +21,39 @@ print ("ROM:", infile, "size:", size, "banks:", int(size / 2048))
 # easier to debug.
 
 if size == 2048:
-  # bank 0
+  # One bank
   rom_table[0x0400:0x0c00] = in_rom[0x0000:0x0800]
-  # mirror bank 0 for unconnected A10
+  # One mirror for A10 lossage
   rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
-  # mirror to fill 16k
+  # Two mirrors to fill 16k
   rom_table[0x1000:0x2000] = rom_table[0x0000:0x1000]
-  # mirror to fill 16k
   rom_table[0x2000:0x4000] = rom_table[0x0000:0x2000]
 
 elif size == 4096:
-  # bank 0
+  # Two banks
   rom_table[0x0400:0x0c00] = in_rom[0x0000:0x0800]
-  # mirror bank 0 for unconnected A10
-  rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
-  # bank 1
   rom_table[0x1400:0x1c00] = in_rom[0x0800:0x1000]
-  # mirror bank 1 for unconnected A10
+  # Two mirrors for A10 lossage
+  rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
   rom_table[0x1c00:0x2000] = rom_table[0x1800:0x1c00]
-  # mirror to fill 16k
+  # One mirror to fill 16k
   rom_table[0x2000:0x4000] = rom_table[0x0000:0x2000]
 
 elif size == 8192:
-  # bank 0
+  # Four banks
   rom_table[0x0400:0x0c00] = in_rom[0x0000:0x0800]
-  # mirror bank 0 for unconnected A10
-  rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
-  # bank 1
   rom_table[0x1400:0x1c00] = in_rom[0x0800:0x1000]
-  # mirror bank 1 for unconnected A10
-  rom_table[0x1c00:0x2000] = rom_table[0x1800:0x1c00]
-  # bank 2
   rom_table[0x2400:0x2c00] = in_rom[0x1000:0x1800]
-  # mirror bank 2 for unconnected A10
-  rom_table[0x2c00:0x3000] = rom_table[0x2800:0x2c00]
-  # bank 3
   rom_table[0x3400:0x3c00] = in_rom[0x1800:0x2000]
-  # mirror bank 3 for unconnected A10
+  # Four mirrors for A10 lossage
+  rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
+  rom_table[0x1c00:0x2000] = rom_table[0x1800:0x1c00]
+  rom_table[0x2c00:0x3000] = rom_table[0x2800:0x2c00]
   rom_table[0x3c00:0x4000] = rom_table[0x3800:0x3c00]
   # Already at 16k, no need to mirror further
 
 else:
-  print("error: can't handle anything bigger than 8k")
+  print("error: can't handle anything not 2k/4k/8k")
   sys.exit(-1)
 
 with open(outfile, "wb") as out_rom:
