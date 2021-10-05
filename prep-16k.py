fda7bc0e5d6b81cfe423bd9cd084275ca122021e
Christopher RYU <software+github@disavowed.jp>
2021-10-05 21:13:16 +0900

    Fix a bunch of thinkos in the 8k code.

--- prep-16k.py
+++ prep-16k.py
@@ -23,7 +23,7 @@ if size == 2048:
   rom_table[0x1000:0x2000] = rom_table[0x0000:0x1000]
   rom_table[0x2000:0x4000] = rom_table[0x0000:0x2000]
 
-elif size >= 4096:
+elif size == 4096:
   # bank 0 (mirror for missing A10)
   rom_table[0x0400:0x0c00] = in_rom[0x0000:0x0800]
   rom_table[0x0c00:0x1000] = rom_table[0x0800:0x0c00]
@@ -42,10 +42,10 @@ elif size == 8192:
   rom_table[0x1c00:0x2000] = rom_table[0x1800:0x1c00]
   # bank 2 (mirror for missing A10)
   rom_table[0x2400:0x2c00] = in_rom[0x1000:0x1800]
-  rom_table[0x2c00:0x2000] = rom_table[0x2800:0x2c00]
+  rom_table[0x2c00:0x3000] = rom_table[0x2800:0x2c00]
   # bank 3 (mirror for missing A10)
   rom_table[0x3400:0x3c00] = in_rom[0x1800:0x2000]
-  rom_table[0x3c00:0x4000] = rom_table[0x2800:0x2c00]
+  rom_table[0x3c00:0x4000] = rom_table[0x3800:0x3c00]
 
 else:
   print("error: can't handle anything bigger than 8k")
